09c182e09af17d09657bd8e341e122cc4921c683
JoseEduardo23 <jose.lincango@epn.edu.ec>
2024-10-10 08:31:36 -0500

    Subiendo la pagina web

---
+++ datosPokemon.js
@@ -0,0 +1,13 @@
+const consultarPokemon = (id, number) => {};
+
+const btnSeleccionar = () => {};
+
+btnSeleccionar();
+
+const lista = document.getElementById("listarpokemon");
+
+const pintarPokemon = (data, id) => {};
+
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_document_getelementsbytagname2
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_document_queryselector
+// https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_setattribute1
